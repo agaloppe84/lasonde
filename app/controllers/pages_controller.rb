dd219b7333cd6d48bc2bc1ade13a780fd1795ee8
Thomas Naude <thomas.naude@gmail.com>
2016-02-29 18:35:08 +0100

    config pundit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
+
+  skip_before_action :authenticate_user!, only: :home
+  skip_after_action :verify_authorized, except: :home, unless: :devise_controller?
+  skip_after_action :verify_policy_scoped, only: :home, unless: :devise_controller?
+
   def home
+
   end
 end
