af8ef95f49f697b9f3d4e1f7f7f394b845aec0cd
aurelien galoppe <agaloppe@me.com>
2016-03-01 15:25:09 +0100

    question controller

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,8 @@ class Question < ActiveRecord::Base
   has_many :iterations
   has_many :items
   validates :label, presence: true, allow_blank: false
+
+  def self.kinds
+    ["single", "multi", "open-ended text", "stars"]
+  end
 end
--- app/policies/question_policy.rb
+++ app/policies/question_policy.rb
@@ -1,7 +1,23 @@
 class QuestionPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope
+      scope.all
     end
+  end
+
+  def show?
+    record.survey.user == user
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    record.survey.user == user
+  end
+
+  def destroy?
+    record.survey.user == user
   end
 end
