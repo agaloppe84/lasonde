4a1a12bad4bee8c2e9df2f6b48bcc1ad926eb0e8
Thomas Naude <thomas.naude@gmail.com>
2016-03-01 17:17:35 +0100

    corrected has_many in iterations

--- app/models/iteration.rb
+++ app/models/iteration.rb
@@ -1,4 +1,4 @@
 class Iteration < ActiveRecord::Base
   belongs_to :question
-  has_many :iterations
+  has_many :answers
 end
--- app/models/respondent.rb
+++ app/models/respondent.rb
@@ -1,3 +1,5 @@
 class Respondent < ActiveRecord::Base
-  has_many :text_answers, :numeric_answers, :item_answers
+  has_many :text_answers
+  has_many :numeric_answers
+  has_many :item_answers
 end
