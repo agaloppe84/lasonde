06981c9c3318ec57a4e227245560fb21aed39770
Thomas Naude <thomas.naude@gmail.com>
2016-03-03 11:02:25 +0100

    ajaxified questionnaire is ok

--- app/controllers/item_answers_controller.rb
+++ app/controllers/item_answers_controller.rb
@@ -1,22 +1,31 @@
 class ItemAnswersController < ApplicationController
 
   def create
-
     @iteration = Iteration.find(params[:iteration_id])
-    item_answer_params[:item_id].each do |item_id|
-      @item_answer = ItemAnswer.new(item_id: item_id, respondent_id: item_answer_params[:respondent_id])
-      authorize @item_answer
-      if @item_answer.save
-        @iteration.answers << @item_answer
-        respond_to do |format|
-          format.html { redirect_to preview_path(@iteration.question.survey) }
-          format.js
+    if check_items
+      item_answer_params[:item_id].each do |item_id|
+        @item_answer = ItemAnswer.new(item_id: item_id, respondent_id: item_answer_params[:respondent_id])
+        authorize @item_answer
+        if @item_answer.save
+          @iteration.answers << @item_answer
         end
       end
+    end
+    authorize @iteration.question
+
+    respond_to do |format|
+      format.html { redirect_to preview_path(@iteration.question.survey) }
+      format.js
     end
   end
 
   private
+
+  def check_items
+    return true if params[:item_answer] && params[:item_answer][:item_id].size > 0
+    @alert = "ça marche pas bien"
+    return false
+  end
 
   def item_answer_params
     params.require(:item_answer).permit(:respondent_id, item_id: [])
