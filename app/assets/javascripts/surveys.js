6bb12366f6835725a0412b3cb3571e6d77dfc35e
Thomas Naude <thomas.naude@gmail.com>
2016-03-04 11:35:53 +0100

    added breadcrumb

--- app/assets/javascripts/surveys.js
+++ app/assets/javascripts/surveys.js
@@ -1,3 +1,14 @@
 $(function(){
   $(".item:first").addClass('active');
 });
+$(function(){
+  $(".bc").on("click", function(e){
+    $('.active').toggleClass('active');
+    $(this).toggleClass('active');
+    $('.bc-content').addClass('hidden');
+    $($(this).data("target")).removeClass('hidden');
+  });
+
+  var hash = window.location.hash;
+  $('.bc a[data-target="'+hash+'"]').click()
+});
